7e96259e6d768bed18de4bdeca0274b035176eb3
MAHER <maherkainaf24@gmail.com>
2026-04-11 20:53:12 +0530

    updated

--- script.js
+++ script.js
@@ -1,47 +1,19 @@
+const filterChips = document.querySelectorAll(".filter-chip");
+const foodItems = document.querySelectorAll(".food-item");
 
-window.addEventListener("scroll", function() {
-  let nav = document.getElementById("navbar");
+filterChips.forEach(btn => {
+  btn.addEventListener("click", () => {
+    const filter = btn.dataset.filter;
 
-  if (window.scrollY > 50) {
-    nav.classList.add("scrolled");
-  } else {
-    nav.classList.remove("scrolled");
-  }
-});
+    filterChips.forEach(b => b.classList.remove("active"));
+    btn.classList.add("active");
 
-
-let toggle = document.getElementById("menu-toggle");
-let links = document.querySelector(".nav-links");
-
-toggle.addEventListener("click", function() {
-  links.classList.toggle("active");
-});
-
-function showVeg() {
-  let vegItems = document.querySelectorAll(".veg");
-  let nonVegItems = document.querySelectorAll(".nonveg");
-
-  vegItems.forEach(item => item.style.display = "inline-block");
-  nonVegItems.forEach(item => item.style.display = "none");
-}
-
-function showNonVeg() {
-  let vegItems = document.querySelectorAll(".veg");
-  let nonVegItems = document.querySelectorAll(".nonveg");
-
-  vegItems.forEach(item => item.style.display = "none");
-  nonVegItems.forEach(item => item.style.display = "inline-block");
-}
-
-let elements = document.querySelectorAll(".fade-up");
-
-window.addEventListener("scroll", () => {
-  elements.forEach(el => {
-    let position = el.getBoundingClientRect().top;
-    let screen = window.innerHeight;
-
-    if (position < screen - 100) {
-      el.classList.add("show");
-    }
+    foodItems.forEach(item => {
+      if (filter === "all" || item.classList.contains(filter)) {
+        item.classList.remove("hide");
+      } else {
+        item.classList.add("hide");
+      }
+    });
   });
 });
