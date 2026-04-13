3b9a87fa95158d0ae31c0ba3f256ae4a99268a78
MAHER <maherkainaf24@gmail.com>
2026-04-13 20:30:50 +0530

    add

--- script.js
+++ script.js
@@ -44,3 +44,10 @@ window.addEventListener("scroll", () => {
     nav.style.padding = "20px 60px";
   }
 });
+
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
